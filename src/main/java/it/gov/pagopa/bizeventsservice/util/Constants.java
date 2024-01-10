2f256d76d7f1503ad597afe3eabbd400e7fc06cf
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2024-01-10 15:44:16 +0100

    PAGOPA-1404 solving sonar issues

--- src/main/java/it/gov/pagopa/bizeventsservice/util/Constants.java
+++ src/main/java/it/gov/pagopa/bizeventsservice/util/Constants.java
@@ -7,5 +7,7 @@ public class Constants {
 
 
     public static final String HEADER_REQUEST_ID = "X-Request-Id";
+    public static final String BIZ_NOT_FOUND_HEADER = "Biz Event not found";
+    public static final String BIZ_NOT_UNIQUE_HEADER = "Biz Event is not unique";
 
 }
