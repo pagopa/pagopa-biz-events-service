42c1c2dcc8a15f955f5b87346b56550e820e3fff
svariant <samuele.varianti@nttdata.com>
2024-01-17 10:38:46 +0100

    [PRDP-316] Added todos as reminder

--- src/main/java/it/gov/pagopa/bizeventsservice/model/transaction/enumeration/OriginType.java
+++ src/main/java/it/gov/pagopa/bizeventsservice/model/transaction/enumeration/OriginType.java
@@ -1,6 +1,6 @@
 package it.gov.pagopa.bizeventsservice.model.transaction.enumeration;
 
 public enum OriginType {
-
+//TODO verify enum for dynamic node ids (es. NDP001PROD)
     INTERNAL, PM, NDP001PROD , NDP002PROD, NDP003PROD
 }
