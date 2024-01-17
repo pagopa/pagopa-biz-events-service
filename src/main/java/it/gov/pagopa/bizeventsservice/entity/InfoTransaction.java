68d6b39148cce34a51a16d9b6b7721ebfc98f4c4
svariant <samuele.varianti@nttdata.com>
2024-01-17 12:58:05 +0100

    [PRDP-316] Updated models

---
+++ src/main/java/it/gov/pagopa/bizeventsservice/entity/InfoTransaction.java
@@ -0,0 +1,19 @@
+package it.gov.pagopa.bizeventsservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class InfoTransaction {
+
+    private String brand;
+    private String brandLogo;
+    private String clientId;
+    private String paymentMethodName;
+    private String type;
+}
