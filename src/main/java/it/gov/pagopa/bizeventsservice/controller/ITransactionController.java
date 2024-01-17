49df5e455b693ae3b527a003c844cdd0ca8434a7
svariant <samuele.varianti@nttdata.com>
2024-01-17 10:33:12 +0100

    [PRDP-316] Added TransactionDetails API definition

--- src/main/java/it/gov/pagopa/bizeventsservice/controller/ITransactionController.java
+++ src/main/java/it/gov/pagopa/bizeventsservice/controller/ITransactionController.java
@@ -1,12 +1,40 @@
 package it.gov.pagopa.bizeventsservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import it.gov.pagopa.bizeventsservice.entity.BizEvent;
+import it.gov.pagopa.bizeventsservice.model.ProblemJson;
+import it.gov.pagopa.bizeventsservice.model.transaction.TransactionDetailResponse;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotBlank;
 
 @Tag(name = "Payment Receipts REST APIs")
 @RequestMapping("/transactions")
 @Validated
 public interface ITransactionController {
 
+    @Operation(summary = "Retrieve the transaction details given its id.", security = {
+            @SecurityRequirement(name = "ApiKey") }, operationId = "getTransactionDetails")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Obtained transaction details.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(name = "TransactionDetailResponse", implementation = TransactionDetailResponse.class))),
+            @ApiResponse(responseCode = "401", description = "Wrong or missing function key.", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "404", description = "Not found the transaction.", content = @Content(schema = @Schema(implementation = ProblemJson.class))),
+            @ApiResponse(responseCode = "422", description = "Unable to process the request.", content = @Content(schema = @Schema(implementation = ProblemJson.class))),
+            @ApiResponse(responseCode = "429", description = "Too many requests.", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Service unavailable.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))) })
+    @GetMapping(value = "/{transaction-id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<TransactionDetailResponse> getTransactionDetails(
+            @RequestHeader("x-fiscal-code") @NotBlank String fiscalCode,
+            @RequestParam(defaultValue = "false", required = false) boolean isCart,
+            @Parameter(description = "The id of the transaction.", required = true) @NotBlank @PathVariable("transaction-id") String transactionId);
 }
--- src/main/java/it/gov/pagopa/bizeventsservice/controller/impl/TransactionController.java
+++ src/main/java/it/gov/pagopa/bizeventsservice/controller/impl/TransactionController.java
@@ -1,9 +1,25 @@
 package it.gov.pagopa.bizeventsservice.controller.impl;
 
 import it.gov.pagopa.bizeventsservice.controller.ITransactionController;
+import it.gov.pagopa.bizeventsservice.model.transaction.TransactionDetailResponse;
+import it.gov.pagopa.bizeventsservice.service.ITransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TransactionController implements ITransactionController {
 
+
+    private ITransactionService transactionService;
+
+    @Autowired
+    public TransactionController(ITransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @Override
+    public ResponseEntity<TransactionDetailResponse> getTransactionDetails(String fiscalCode, boolean isCart, String transactionId) {
+        return null;
+    }
 }
