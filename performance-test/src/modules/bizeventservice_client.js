dc220788b798768341b014d272c36309c20d217b
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-01-18 11:49:59 +0100

    [PRDP-319] feat: Introducing get_transaction_details.js and get_transaction_list.js performance test scenarios, updated helpers and clients to manage biz-event for transaction test

--- performance-test/src/modules/bizeventservice_client.js
+++ performance-test/src/modules/bizeventservice_client.js
@@ -3,3 +3,11 @@ import http from 'k6/http';
 export function getOrganizationReceipt(bizEventServiceURI, organizationfiscalcode, iur, iuv, params) {
     return http.get(bizEventServiceURI+`organizations/${organizationfiscalcode}/receipts/${iur}/paymentoptions/${iuv}`, params)
 }
+
+export function getTransactionList(bizEventServiceURI, params) {
+    return http.get(bizEventServiceURI+`transactions?start=0&size=5`, params)
+}
+
+export function getTransactionDetails(bizEventServiceURI, id, params) {
+    return http.get(bizEventServiceURI+`transactions/${id}`, id , params)
+}
--- performance-test/src/modules/helpers.js
+++ performance-test/src/modules/helpers.js
@@ -29,5 +29,20 @@ export function getRandomItemFromArray(items) {
 	return items[Math.floor(Math.random()*items.length)];
 }
 
+export function makeRandomFiscalCode() {
+
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numbers = '0123456789';
+  var charactersLength = characters.length;
+  var numbersLength = numbers.length;
+
+  var charValue = characters.charAt(Math.floor(Math.random() * charactersLength));
+  var numValue = numbers.charAt(Math.floor(Math.random() * numbersLength));
+
+  return charValue.repeat(6) + numValue.repeat(2) + charValue
+  + numValue.repeat(2) + charValue + numValue.repeat(3) + charValue;
+
+}
+
 
 
