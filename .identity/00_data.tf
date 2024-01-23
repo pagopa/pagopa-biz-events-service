9eae0f2376967161dffd4aa42851809a05badbca
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-01-23 10:12:03 +0100

    [PRDP-331] feat: Updated to federated identity

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -3,6 +3,11 @@ data "azurerm_storage_account" "tfstate_app" {
   resource_group_name = "io-infra-rg"
 }
 
+data "azurerm_storage_account" "tfstate_app" {
+  name                = "pagopainfraterraform${var.env}"
+  resource_group_name = "io-infra-rg"
+}
+
 data "azurerm_resource_group" "dashboards" {
   name = "dashboards"
 }
