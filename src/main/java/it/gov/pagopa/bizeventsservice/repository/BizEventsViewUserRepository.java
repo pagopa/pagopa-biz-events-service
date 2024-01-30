97e4b37ebe5d792b1dae8a87bdbb67a7d08310fb
svariant <samuele.varianti@nttdata.com>
2024-01-30 15:56:37 +0100

    [PRDP-349] Changed view user query to sliced

--- src/main/java/it/gov/pagopa/bizeventsservice/repository/BizEventsViewUserRepository.java
+++ src/main/java/it/gov/pagopa/bizeventsservice/repository/BizEventsViewUserRepository.java
@@ -3,16 +3,17 @@ package it.gov.pagopa.bizeventsservice.repository;
 import com.azure.spring.data.cosmos.repository.CosmosRepository;
 import com.azure.spring.data.cosmos.repository.Query;
 import it.gov.pagopa.bizeventsservice.entity.view.BizEventsViewUser;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 /**
  * Repository interface for biz-events-view-user collection
  */
 @Repository
 public interface BizEventsViewUserRepository extends CosmosRepository<BizEventsViewUser, String> {
-    @Query("select * from c where c.fiscalCode = @fiscalCode")
-    List<BizEventsViewUser> getBizEventsViewUserByFiscalCode(@Param("fiscalCode") String fiscalCode);
+    @Query("select * from c where c.fiscalCode = @fiscalCode order by c.transactionDate desc")
+    Slice<BizEventsViewUser> getBizEventsViewUserByFiscalCode(@Param("fiscalCode") String fiscalCode, Pageable pageable);
 }
