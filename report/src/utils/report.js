ede6e027f400fbd1c7bc23f6a2be30946ee05bbb
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2024-07-29 23:52:17 +0200

    fix

--- report/src/utils/report.js
+++ report/src/utils/report.js
@@ -33,8 +33,8 @@ let reportT = '{"text":"📈 Ingestion BizView 2024 :\\n", "months":[\
 report = JSON.parse(reportT);
 // console.log(report);
 
-// const months4Year = [6,5,4,3,2,1]; // giu'24 to gen'24
-const months4Year = [7]; // giu'24 to gen'24
+const months4Year = [6,5,4,3,2,1]; // giu'24 to gen'24
+// const months4Year = [7]; // giu'24 to gen'24
 
 
 // [
