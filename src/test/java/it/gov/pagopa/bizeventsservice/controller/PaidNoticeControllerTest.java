36618c8ea6f5e011b25ae237294391c6384023d2
Jacopo Carlini <jacopo.carlini@pagopa.it>
2024-09-04 10:09:07 +0200

    junit coverage

--- src/test/java/it/gov/pagopa/bizeventsservice/controller/PaidNoticeControllerTest.java
+++ src/test/java/it/gov/pagopa/bizeventsservice/controller/PaidNoticeControllerTest.java
@@ -1,11 +1,13 @@
 package it.gov.pagopa.bizeventsservice.controller;
 
 
+import it.gov.pagopa.bizeventsservice.entity.BizEvent;
 import it.gov.pagopa.bizeventsservice.exception.AppError;
 import it.gov.pagopa.bizeventsservice.exception.AppException;
 import it.gov.pagopa.bizeventsservice.model.response.transaction.TransactionDetailResponse;
 import it.gov.pagopa.bizeventsservice.model.response.transaction.TransactionListItem;
 import it.gov.pagopa.bizeventsservice.model.response.transaction.TransactionListResponse;
+import it.gov.pagopa.bizeventsservice.service.IBizEventsService;
 import it.gov.pagopa.bizeventsservice.service.ITransactionService;
 import it.gov.pagopa.bizeventsservice.util.Utility;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,15 +16,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.io.IOException;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -33,9 +40,13 @@ public class PaidNoticeControllerTest {
     public static final String VALID_FISCAL_CODE = "AAAAAA00A00A000A";
     public static final String FISCAL_CODE_HEADER_KEY = "x-fiscal-code";
     public static final String PAIDS_EVENT_ID_DISABLE_PATH = "/paids/1234321234/disable";
+    public static final String TRANSACTION_RECEIPT_PATH = "/transactions/event-id/pdf";
 
     @Autowired
     private MockMvc mvc;
+
+    @MockBean
+    private IBizEventsService bizEventsService;
 
     @MockBean
     private ITransactionService transactionService;
@@ -83,6 +94,39 @@ public class PaidNoticeControllerTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
                 .andReturn();
+    }
+
+    @Test
+    void getPDFReceipt_ShouldReturnOK() throws Exception {
+
+        BizEvent bizEvent = mock (BizEvent.class);
+        when (bizEventsService.getBizEvent(anyString())).thenReturn(bizEvent);
+
+        MvcResult result = mvc.perform(get(TRANSACTION_RECEIPT_PATH)
+                        .header(FISCAL_CODE_HEADER_KEY, VALID_FISCAL_CODE)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_PDF))
+                .andReturn();
+
+        verify(bizEventsService).getBizEvent("event-id");
+        verify(transactionService).getPDFReceipt(VALID_FISCAL_CODE,"event-id");
+        assertEquals(receipt.length, result.getResponse().getContentAsByteArray().length);
+    }
+
+    @Test
+    void getPDFReceiptForOldPMEvent_ShouldReturnNOTFOUND() throws Exception {
+        AppException ex = new AppException(HttpStatus.NOT_FOUND, "mock", "mock");
+        when (bizEventsService.getBizEvent(anyString())).thenThrow(ex);
+
+        mvc.perform(get(TRANSACTION_RECEIPT_PATH)
+                        .header(FISCAL_CODE_HEADER_KEY, VALID_FISCAL_CODE)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        verify(bizEventsService).getBizEvent("event-id");
     }
 
 }
