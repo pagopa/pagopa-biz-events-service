f813beee5bee768932fe057fda98f2bcc5f79b38
svariant <samuele.varianti@nttdata.com>
2026-01-28 17:06:32 +0100

    fix: [PIDM-1421] ReceiptService fiscal code as header

--- src/main/java/it/gov/pagopa/bizeventsservice/client/IReceiptGetPDFClient.java
+++ src/main/java/it/gov/pagopa/bizeventsservice/client/IReceiptGetPDFClient.java
@@ -8,16 +8,16 @@ import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(value = "getReceiptPDF", url = "${service.get.pdf.receipt.host}", configuration = PDFGetReceiptFeignConfig.class)
 public interface IReceiptGetPDFClient {
 
     @Retryable(
-            exclude = FeignException.FeignClientException.class,
+            exclude = FeignException.class,
             maxAttemptsExpression = "${get.pdf.retry.maxAttempts}",
             backoff = @Backoff(delayExpression = "${get.pdf.retry.maxDelay}"))
     @GetMapping(value = "/pdf/{third-party-id}")
-    ResponseEntity<byte[]> getReceiptPdf(@PathVariable("third-party-id") String thirdPartyId, @RequestParam("fiscal_code") String fiscalCode);
+    ResponseEntity<byte[]> getReceiptPdf(@RequestHeader("fiscal_code") String fiscalCode, @PathVariable("third-party-id") String thirdPartyId);
 
 }
