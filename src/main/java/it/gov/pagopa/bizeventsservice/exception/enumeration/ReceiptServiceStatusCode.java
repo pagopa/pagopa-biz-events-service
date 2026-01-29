36622be38679cdff285709070515050b9f9bd523
svariant <samuele.varianti@nttdata.com>
2026-01-29 15:20:22 +0100

    fix: [PIDM-1421] Update receiptservice client error descriptions

--- src/main/java/it/gov/pagopa/bizeventsservice/exception/enumeration/ReceiptServiceStatusCode.java
+++ src/main/java/it/gov/pagopa/bizeventsservice/exception/enumeration/ReceiptServiceStatusCode.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 public enum ReceiptServiceStatusCode {
     PDFS_706("PDFS_706", "Fiscal code not authorized to access the requested receipt document"),
 
-    PDFS_714("PDFS_714", "The PDF has not been generated yet"),
-    PDFS_715("PDFS_715", "The PDF failed to be generated, retry is possible"),
-    PDFS_716("PDFS_716", "The PDF failed to be generated, review is needed"),
+    PDFS_714("PDFS_714", "The PDF has not been generated yet."),
+    PDFS_715("PDFS_715", "The PDF generation failed. A retry is possible."),
+    PDFS_716("PDFS_716", "The PDF generation failed. Manual review is required."),
 
     PDFS_800("PDFS_800", "Receipt not found with the provided third party id"),
     PDFS_801("PDFS_801", "Cart not found with the provided third party id");
