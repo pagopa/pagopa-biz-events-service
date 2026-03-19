2cba6dd50a45a707e69f95a733f7eed75cb74bf8
Francesco Di Tonno <francesco.ditonno.ext@jakala.com>
2026-03-19 17:30:23 +0100

    Revert "feat: search transactions cucumber"
    This reverts commit 0a98b7014b12946ee024537256a933d7bfcd85b5.

--- .identity/03_github_environment.tf
+++ .identity/03_github_environment.tf
@@ -30,8 +30,7 @@ locals {
     "RECEIPT_COSMOS_DB_CONN_STRING" : "AccountEndpoint=https://pagopa-${var.env_short}-${local.location_short}-receipts-ds-cosmos-account.documents.azure.com:443/;AccountKey=${data.azurerm_cosmosdb_account.receipts_cosmos.primary_key};",
     "RECEIPTS_STORAGE_CONN_STRING" : data.azurerm_storage_account.receipts_sa.primary_connection_string,
     "SUBKEY" : data.azurerm_key_vault_secret.key_vault_integration_test_subkey.value,
-    "TOKENIZER_API_KEY" : data.azurerm_key_vault_secret.key_vault_receipts_tokenizer_api_key.value,
-    "SEARCH_TRANSACTION_TOKEN" : data.azurerm_key_vault_secret.key_vault_search_transactions_token_secret.value,
+    "TOKENIZER_API_KEY" : data.azurerm_key_vault_secret.key_vault_receipts_tokenizer_api_key.value
   }
   env_variables = {
     "CONTAINER_APP_ENVIRONMENT_NAME" : local.container_app_environment.name,
